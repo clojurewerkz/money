b7f0a2052a15b52ff09d78c42de48aac7b4e46b2
Michael S. Klishin <michael@novemberain.com>
2012-04-26 21:22:12 +0400

    Add Sonatype OSS repositories

--- project.clj
+++ project.clj
@@ -6,8 +6,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.joda/joda-money "0.6"]]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :aliases { "all" ["with-profile" "dev:dev,1.4"] }
+  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
+  :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
+                             :snapshots false
+                             :releases {:checksum :fail :update :always}}
+                 "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                             :snapshots true
+                             :releases {:checksum :fail :update :always}}}  
+  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5"]}
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :warn-on-reflection true)
