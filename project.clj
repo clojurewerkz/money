0c06d385921c408ee0e598cb37383163997503de
Michael Klishin <michael@defprotocol.org>
2013-03-24 22:57:32 +0400

    Add Codox

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.joda/joda-money "0.8"]]
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+  :profiles {:dev {:plugins [[codox "0.6.4"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
              :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
@@ -14,9 +15,9 @@
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
-                             :snapshots true
-                             :releases {:checksum :fail :update :always}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
+  :aliases  {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :warn-on-reflection true)
