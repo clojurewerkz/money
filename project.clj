67e91039c37168dca92734ab2f63ea9cae4fd2a5
Michael Klishin <michael@defprotocol.org>
2013-04-02 01:24:56 +0400

    1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/money "1.0.0-beta3-SNAPSHOT"
+(defproject clojurewerkz/money "1.0.0"
   :description "A Clojure library that deals with monetary values and currencies. Built on top of Joda Money."
   :min-lein-version "2.0.0"
   :url "http://github.com/clojurewerkz/money"
