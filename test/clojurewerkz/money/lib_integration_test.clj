120561f05c2770a077ee6e4dde34527c03d53e32
Michael Klishin <michael@defprotocol.org>
2013-04-24 20:46:01 +0400

    Update these (duplicated) tests, too

--- test/clojurewerkz/money/lib_integration_test.clj
+++ test/clojurewerkz/money/lib_integration_test.clj
@@ -10,12 +10,12 @@
   (testing "without minor units"
     (let [m (cm/amount-of cc/USD 20)]
       (is (= (json/encode m)
-             "\"USD 20.0\""))))
+             "\"USD20,00\""))))
   (testing "with a currency without minor units"
     (let [m (cm/of-major cc/JPY 2000)]
       (is (= (json/encode m)
-             "\"JPY 2000\""))))
+             "\"JPY2.000\""))))
   (testing "with minor units"
     (let [m (cm/amount-of cc/USD 20.5)]
       (is (= (json/encode m)
-             "\"USD 20.50\"")))))
+             "\"USD20,50\"")))))
