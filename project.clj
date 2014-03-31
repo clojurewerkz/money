14e04f81ff4dfe525055161dd9f56a95da9acb71
Michael Klishin <michael@clojurewerkz.org>
2014-03-31 08:46:46 +0400

    1.5.1 is an extra profile now

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                                   [cheshire               "5.3.1"]
                                   [hiccup                 "1.0.3"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
@@ -19,7 +19,7 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.6:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+master"]}
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :resource-paths    ["src/resources"]
