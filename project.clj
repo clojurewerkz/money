ee77b67b30e9b2a2298860bc0c05e1cf58d80f6e
Michael Klishin <michael@clojurewerkz.org>
2014-06-05 21:54:12 +0400

    Codox 0.8.9

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.joda/joda-money "0.8"]]
-  :profiles {:dev {:plugins      [[codox "0.8.8"]]
+  :profiles {:dev {:plugins      [[codox "0.8.9"]]
                    :dependencies [[com.novemberain/monger "1.7.0"]
                                   [cheshire               "5.3.1"]
                                   [hiccup                 "1.0.3"]]}
