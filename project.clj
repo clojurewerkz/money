eba200c62e7a9e66b928cd599d8cbb2beb731122
Michael Klishin <michael@clojurewerkz.org>
2014-11-07 22:52:00 +0300

    Joda Money 0.10.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.joda/joda-money "0.9.1"]]
+                 [org.joda/joda-money "0.10.0"]]
   :profiles {:dev {:plugins      [[codox "0.8.10"]]
                    :dependencies [[com.novemberain/monger "2.0.0-rc1"]
                                   [cheshire               "5.3.1"]
