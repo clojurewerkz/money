e43ff746e58690b122b26c5c8ac835fef2c66307
Michael Klishin <michael@clojurewerkz.org>
2014-11-07 23:07:49 +0300

    Test against 1.7.0 preview releases

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
                                   [hiccup                 "1.0.3"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha3"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
@@ -19,7 +20,7 @@
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+1.7:+master"]}
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :resource-paths    ["src/resources"]
