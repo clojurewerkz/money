39cb508b10a8096ef0a5a526953752e7880b2fc4
Michael Klishin <michael@clojurewerkz.org>
2015-05-13 02:02:28 +0300

    For realz

--- project.clj
+++ project.clj
@@ -10,8 +10,6 @@
                    :dependencies [[com.novemberain/monger "2.0.0"]
                                   [cheshire               "5.3.1"]
                                   [hiccup                 "1.0.3"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
