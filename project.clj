641c4c0beaf97f27c0deb713d14cff5451e6579f
Michael Klishin <michael@clojurewerkz.org>
2015-07-11 17:03:06 +0300

    Depend on Clojure 1.7

--- project.clj
+++ project.clj
@@ -4,21 +4,21 @@
   :url "http://github.com/clojurewerkz/money"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.joda/joda-money "0.10.0"]]
   :profiles {:dev {:plugins      [[codox "0.8.10"]]
                    :dependencies [[com.novemberain/monger "2.0.0"]
                                   [cheshire               "5.3.1"]
                                   [hiccup                 "1.0.3"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-beta2"]]}
-             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
                  "sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
                                        :snapshots true
                                        :releases {:checksum :fail :update :always}}}
-  :aliases  {"all" ["with-profile" "+dev:+1.7:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.6:+master"]}
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
   :resource-paths    ["src/resources"]
